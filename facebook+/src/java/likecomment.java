f831d2ca3534ca3fb657dda814e52f5f16782955
Leon Luo <17133708@qq.com>
2015-04-29 12:15:15 -0400

    like post & comment

---
+++ facebook+/src/java/likecomment.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Leon
+ */
+public class likecomment extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String userid = (String) request.getSession().getAttribute("userid");
+       // String pid = (String) request.getSession().getAttribute("pid");
+        String cid=request.getParameter("cid");
+        PrintWriter out = response.getWriter();
+        if (userid == null || cid == null) {
+            out.println("<script language=\"JavaScript\">alert(\"please login first！\");self.location='index.html';</script>");
+        } else {
+            PreparedStatement ps = null;
+
+            try {
+
+                String dburl = "jdbc:mysql://mysql2.cs.stonybrook.edu:3306/fhonda?user=fhonda&password=108180831";
+                String driver = "com.mysql.jdbc.Driver";
+                Class.forName(driver).newInstance(); //init driver
+                Connection conn = DriverManager.getConnection(dburl);
+                ps = conn.prepareStatement("INSERT INTO user_likes_comment(User,Comment) values(?,?)");
+                ps.setString(1, userid);
+                ps.setString(2, cid);
+                ps.execute();  //execute the query
+
+                response.sendRedirect("post_page.jsp");
+
+            } catch (Exception e) {
+            } finally {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(login_check.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+
+    }
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+    
